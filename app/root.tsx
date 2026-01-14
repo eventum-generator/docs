5db6eeef0336fc075247e5c06df3c3bcb52fb8f9
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2026-01-14 23:56:29 +0300

    Add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -13,6 +13,7 @@ import './app.css';
 import SearchDialog from '@/components/search';
 
 export const links: Route.LinksFunction = () => [
+  { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
   {
     rel: 'preconnect',
