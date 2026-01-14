3aac3fcafe59bab78503240109cb3e477f880979
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2026-01-15 00:24:40 +0300

    Add icon and title to header

--- app/lib/layout.shared.tsx
+++ app/lib/layout.shared.tsx
@@ -3,7 +3,17 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'React Router',
+      title: (
+        <div className="flex flex-row gap-3 items-center">
+          <img
+            src="/logo.svg"
+            alt="Eventum Logo"
+            width="auto"
+            className="h-[25px] w-auto object-contain"
+          />
+          <p>Eventum</p>
+        </div>
+      ),
     },
   };
 }
